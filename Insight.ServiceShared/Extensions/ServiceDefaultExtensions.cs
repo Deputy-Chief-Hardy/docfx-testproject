7ff830630de71793ce1a378a614721e40e88a88f
Leonhard Stocker <49750085+Deputy-Chief-Hardy@users.noreply.github.com>
2024-02-27 13:55:43 +0100

    added files part1

---
+++ Insight.ServiceShared/Extensions/ServiceDefaultExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+namespace KnorrBremse.Insight.ServiceShared.Extensions;
+
+public static class ServiceDefaultExtensions
+{
+	/// <summary>
+	/// Adds default services for all backend APIs.
+	/// </summary>
+	public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
+	{
+		builder.AddDefaultHealthChecks();
+
+		builder.Services.ConfigureHttpClientDefaults(http =>
+		{
+			// Turn on resilience by default
+			http.AddStandardResilienceHandler();
+		});
+
+		return builder;
+	}
+
+	/// <summary>
+	/// Adds a default health check service.
+	/// </summary>
+	public static IHostApplicationBuilder AddDefaultHealthChecks(this IHostApplicationBuilder builder)
+	{
+		builder.Services.AddHealthChecks()
+			// Add a default liveness check to ensure app is responsive
+			.AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
+
+		return builder;
+	}
+
+	/// <summary>
+	/// Map the default endpoints for health checks, namely "/health" and "/liveness".
+	/// </summary>
+	public static WebApplication MapDefaultEndpoints(this WebApplication app)
+	{
+		// All health checks must pass for app to be considered ready to accept traffic after starting
+		app.MapHealthChecks("/health");
+
+		// Only health checks tagged with the "live" tag must pass for app to be considered alive
+		app.MapHealthChecks("/liveness", new HealthCheckOptions
+		{
+			Predicate = r => r.Tags.Contains("live")
+		});
+
+		return app;
+	}
+}
---
+++ Insight.ServiceShared/Persistence/TableNameWithoutEntityConvention.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace KnorrBremse.Insight.ServiceShared.Persistence;
+
+/// <summary>
+///		A convention that configures the table name from the class name by removing the suffix "Entity". 
+/// </summary>
+/// <remarks>
+///		In the Insight project, all entity classes are named "{table-name}Entity". 
+///		So this convention removes "Entity", leaving just the original table name.
+///		<br />
+///		<example>e.g. class <c>AssemblyLineEntity</c> maps to table <c>AssemblyLine</c>.</example>
+/// </remarks>
+public class TableNameWithoutEntityConvention : IModelFinalizingConvention
+{
+	/// <inheritdoc cref="TableNameWithoutEntityConvention"/>
+	public TableNameWithoutEntityConvention() {	}
+
+	public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder,
+		IConventionContext<IConventionModelBuilder> context)
+	{
+		foreach (var entityType in modelBuilder.Metadata.GetEntityTypes())
+		{
+			var tableName = entityType.GetTableName();
+			if (tableName != null && tableName.EndsWith("Entity"))
+			{
+				entityType.SetTableName(tableName.Replace("Entity", ""));
+			}
+		}
+	}
+}
