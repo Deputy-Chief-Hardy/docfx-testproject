df37c395adeb3200f3eb0ccaca759f0e40cfc1c2
Leonhard Stocker <Leonhard.Stocker@msg.group>
2024-02-27 14:29:38 +0100

    deleted all files

--- Insight.Web/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace KnorrBremse.InsightWeb.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
